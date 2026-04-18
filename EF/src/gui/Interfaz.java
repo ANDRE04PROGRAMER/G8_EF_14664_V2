350beb7560acb0845f7f14b0f138ff5a931f6f72
Jair751 <jairsebastianchl@gmail.com>
2026-04-18 08:45:27 -0500

    pantalla_inicio

--- EF/src/gui/Interfaz.java
+++ EF/src/gui/Interfaz.java
@@ -1,8 +1,7 @@
+
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,27 +12,23 @@ import clases.Producto;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.crypto.AEADBadTagException;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
-import java.awt.ScrollPane;
-import javax.swing.JScrollPane;
-import javax.swing.JScrollBar;
 
 public class Interfaz extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel lblNewLabel;
 	private JTextField txtProducto;
-	private JButton btnNewButton;
 	private JTextArea txtS;
-	
-	/**
-	 * Launch the application.
-	 */
+
+	private JButton btnBuscar;
+	private JButton btnReportar;
+	private JButton btnAdicionar;
+
+	ArregloProducto ae = new ArregloProducto();
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -48,10 +43,6 @@ public class Interfaz extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	
 	public Interfaz() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -59,71 +50,93 @@ public class Interfaz extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnReportar = new JButton("Reportar");
-		btnReportar.setBounds(20, 50, 84, 20);
-		contentPane.add(btnReportar);
-		
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(109, 50, 84, 20);
-		contentPane.add(btnBuscar);
-		
-		JButton btnAdicionar = new JButton("Adicionar");
-		btnAdicionar.setBounds(203, 50, 84, 20);
-		contentPane.add(btnAdicionar);
-		
-		txtProducto = new JTextField();//
-		txtProducto.setBounds(97, 10, 96, 18);
+
+		// Label
+		JLabel lblProducto = new JLabel("Producto:");
+		lblProducto.setBounds(20, 13, 80, 14);
+		contentPane.add(lblProducto);
+
+		// TextField
+		txtProducto = new JTextField();
+		txtProducto.setBounds(97, 10, 96, 20);
 		contentPane.add(txtProducto);
-		txtProducto.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Producto:");//
-		lblNewLabel.setBounds(20, 13, 44, 12);
-		contentPane.add(lblNewLabel);
-		
-		JTextArea txtS = new JTextArea();
+
+		// Área de texto
+		txtS = new JTextArea();
 		txtS.setBounds(10, 93, 416, 160);
 		contentPane.add(txtS);
 
+		// Botón Reportar
+		btnReportar = new JButton("Reportar");
+		btnReportar.setBounds(20, 50, 84, 23);
+		contentPane.add(btnReportar);
+
+		btnReportar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_btnReportar_actionPerformed(e);
+			}
+		});
+
+		// Botón Buscar
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(109, 50, 84, 23);
+		contentPane.add(btnBuscar);
+
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_btnBuscar_actionPerformed(e);
+			}
+		});
+
+		// Botón Adicionar
+		btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.setBounds(203, 50, 100, 23);
+		contentPane.add(btnAdicionar);
+
+		btnAdicionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				do_btnAdicionar_actionPerformed(e);
+			}
+		});
 	}
-	
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnBuscar) {
-			do_btnBuscar_actionPerformed(e);
-		}
-		if (e.getSource() == btnNewButton) {
-			do_btnNewButton_actionPerformed(e);
-		}
-	}
-	/*-----------------------*/
-	ArregloProducto ae =new ArregloProducto();
-	private JScrollPane scrollPane;
-	private JButton btnBuscar;
-	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+
+	// ---------------- FUNCIONES ----------------
+
+	protected void do_btnReportar_actionPerformed(ActionEvent e) {
 		txtS.setText("");
 		Listado();
-		
 	}
+
+	protected void do_btnBuscar_actionPerformed(ActionEvent e) {
+		try {
+			int codigo = Integer.parseInt(txtProducto.getText());
+			Producto p = ae.Buscar(codigo);
+
+			if (p != null) {
+				JOptionPane.showMessageDialog(this, "Sí existe");
+			} else {
+				JOptionPane.showMessageDialog(this, "No existe");
+			}
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(this, "Ingrese un código válido");
+		}
+	}
+
+	protected void do_btnAdicionar_actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(this, "Función adicionar aún no implementada");
+	}
+
 	void Listado() {
 		Imprimir("Codigo\tNombre\tMarca\tCategoria\tStock\tPrecio Costo\tPrecio Venta");
-		for (int i = 0;i<ae.Tamaño();i++) {
-			Imprimir(""+ae.Obtener(i).getCodigo()+"\t"+ae.Obtener(i).getNombre()
-					+"\t"+ae.Obtener(i).getMarca()+"\t"+ae.Obtener(i).getCategoria()
-					+"\t"+ae.Obtener(i).getStock()+"\t"+ae.Obtener(i).getPrecioC()
-					+"\t"+ae.Obtener(i).getPrecioV());
+		for (int i = 0; i < ae.Tamaño(); i++) {
+			Producto p = ae.Obtener(i);
+			Imprimir(p.getCodigo() + "\t" + p.getNombre() + "\t" + p.getMarca()
+					+ "\t" + p.getCategoria() + "\t" + p.getStock()
+					+ "\t" + p.getPrecioC() + "\t" + p.getPrecioV());
 		}
 	}
-	
+
 	void Imprimir(String s) {
-		txtS.append(s+"\n");
-	}
-	protected void do_btnBuscar_actionPerformed(ActionEvent e) {
-		int codigo = Integer.parseInt(txtProducto.getText());
-		Producto p = ae.Buscar(codigo);
-		if (p != null) {
-			JOptionPane.showMessageDialog(this, "Si existe");
-		}else {
-			JOptionPane.showMessageDialog(this, "No existe");
-		}
+		txtS.append(s + "\n");
 	}
 }
